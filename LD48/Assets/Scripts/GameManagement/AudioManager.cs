abfa50684a1256bf465d59bf92f0b7b9bc6bc338
Dhruv-Python <59411426+Dhruv-Python@users.noreply.github.com>
2021-04-27 05:23:21 +0530

    shr

---
+++ LD48/Assets/Scripts/GameManagement/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Game.GameManagement
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public static KeyManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null) Destroy(gameObject);
+            Instance = this;
+            DontDestroyOnLoad(transform.root);
+        }
+
+        public void PlayMusic(AudioClip){
+
+        }
+    }
+}
